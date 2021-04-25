6865dabd1e081748e81369af74c285a04b3dad83
Cesar Bonilla <cefeboru@gmail.com>
2021-04-25 00:17:06 -0600

    Expose jobs api env var to the bundled app

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  jobsApiUrl: process.env.JOBS_API_URL || "http://localhost:3000/jobs",
+  jobsApiUrl: process.env.REACT_APP_JOBS_API_URL || "http://localhost:3000/jobs",
 };
 
 export default config;
